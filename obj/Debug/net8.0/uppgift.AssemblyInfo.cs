91ba7ee834dc02a4a7eae66c64418054e35a8148
Kriitzyy <Akadmn04@gmail.com>
2025-02-16 00:56:14 +0100

    Synkade med GitHub och löste konflikter

--- obj/Debug/net8.0/uppgift.AssemblyInfo.cs
+++ obj/Debug/net8.0/uppgift.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uppgift")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674c1995f00f53e7e3d141421217b678312bc1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("uppgift")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uppgift")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
